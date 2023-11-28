c8fb5ec54e9bcfed964131c23a01a744b2ec39c2
ömer <ozcanomer15429@gmail.com>
2023-11-28 22:42:31 +0300

    dockerfile oluşturuldu

--- src/main/java/com/example/Account/dto/converter/AccountDtoConverter.java
+++ src/main/java/com/example/Account/dto/converter/AccountDtoConverter.java
@@ -2,11 +2,13 @@ package com.example.Account.dto.converter;
 
 import com.example.Account.dto.AccountDto;
 import com.example.Account.model.Account;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Component
 public class AccountDtoConverter {
 
     private final CustomerDtoConverter customerDtoConverter;
